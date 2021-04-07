5d2230a2732ef9152c668c81ae5bd2ad59a4ecf8
Vergiliogu <vergiliopoleza@gmail.com>
2021-04-07 08:07:43 -0300

    OH

--- 8 Funções/apoio.py
+++ 8 Funções/apoio.py
@@ -21,7 +21,7 @@ def teste(nome, idade):
 teste("Gustavo", 100) # Retorna "Seu nome é Gustavo e sua idade é 100"
 
 
-# Definindo argumentos com valor padrão
+# Definindo parâmetros com valor padrão
 def teste(nome, idade=None):
     if idade:
         frase = nome + ", você tem " + str(idade) + " anos"
@@ -31,3 +31,32 @@ def teste(nome, idade=None):
 
 teste("Gustavo") # Retorna "Olá Gustavo"
 teste("Gustavo", 100) # Retorna "Gustavo, você tem 100 anos"
+
+# Parâmetros com valor padrão devem ser colocados sem no final
+def teste(nome, idade, cpf=None, altura=None):
+    pass
+
+
+# Definindo **kwargs
+def teste(**kwargs):
+    kwargs # Retornará {'nome': 'Gustavo', 'idade': 1}
+# **kwargs devem sempre ser definidos no final
+
+teste(nome="Gustavo", idade=1)
+
+
+# É possível passar qualquer objeto como um argumento
+def teste(numeros):
+    numeros # Retorna [1, 2, 3]
+
+lista = [1, 2, 3]
+teste(lista)
+
+
+# Definindo o tipo do parâmetro e o tipo de saída da função
+def teste(nome: str, idade: int) -> tuple:
+    return nome, idade # Retorna ("Gustavo", 100)
+
+teste("Gustavo", 100)
+
+# Caso queira colcar o tipo do parâmetro e um valor padrão, a ordem é (parâmetro: tipo = valor_padrão)
