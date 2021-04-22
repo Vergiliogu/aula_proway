3ed208d2511e68ea0f33b0bfdbc22a95c515da79
Vergiliogu <vergiliopoleza@gmail.com>
2021-04-22 09:44:16 -0300

    A

--- 17 Testes Unitários/tests/test_bora.py
+++ 17 Testes Unitários/tests/test_bora.py
@@ -1,10 +1,26 @@
-import unittest
+from unittest import TestCase
 from funcoes.bora import *
 
 
-class TestBora(unittest.TestCase):
+class TestandoBora(TestCase):
 
-    def test_soma_funciona(self):
+    def test_soma_is_working(self):
         n1 = 10
-        n2 = 20
-        self.assertEqual(soma(n1, n2), 30)
+        n2 = 12
+        self.assertEqual(soma(n1, n2), 22)
+
+    def test_raiz_ta_funfando(self):
+
+        type_test = "A"
+        with self.assertRaises(TypeError):
+            raiz(type_test)
+
+        value_test = -9
+        with self.assertRaises(ValueError):
+            raiz(value_test)
+
+        self.assertEqual(raiz(144), 12)
+
+    def test_lista(self):
+        self.assertTrue(listadef(0))
+        self.assertFalse(listadef(1))
